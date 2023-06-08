5afd3d6175c6bf1b883bf86c60af82f5e5063106
Nav-3299 <70155782+Nav-3299@users.noreply.github.com>
2023-06-08 11:41:12 +0530

    add framework

---
+++ NavValidator/NavValidator/NavValidator.h
@@ -0,0 +1,18 @@
+//
+//  NavValidator.h
+//  NavValidator
+//
+//  Created by 3Embed on 08/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NavValidator.
+FOUNDATION_EXPORT double NavValidatorVersionNumber;
+
+//! Project version string for NavValidator.
+FOUNDATION_EXPORT const unsigned char NavValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavValidator/PublicHeader.h>
+
+
---
+++ NavValidator/Validator.swift
@@ -0,0 +1,13 @@
+//
+//  Validator.swift
+//  NavValidator
+//
+//  Created by 3Embed on 08/06/23.
+//
+
+import Foundation
+public struct Validator {
+    public static func displayMessage(){
+        print("Hello I'm Framework")
+    }
+}
